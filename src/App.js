70e4a911f59270971406d438fab75b36e0571721
Jose A. Campos <jjoseccampos@gmail.com>
2019-03-16 11:52:33 -0400

    navbar displaying

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
-import FirstClass from './components/FirstClass'
-import Cacao from './components/Cacao'
-
-const taco = [
-  "lettuce",
-  "cheese",
-  "tomatoe",
-  "sour cream",
-  "corn shell"
-];
-
-const choco = [
-  "72% chocolate",
-  "milk chocolate",
-  "white chocolate",
-  "dark chocolate",
-  "hazelnut chocolate"
-]
+import Cats from './components/cats/cats'
+import Navbar from './components/navbar/navbar'
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* {taco.map(taco => {
-          return <FirstClass name={taco}/>
-        })}
-        <HelloWorld />
-        <FirstClass name="Stella" />
-        <FirstClass name="Bree" />
-        <FirstClass name="Adrian" />
-        <FirstClass name="Liz" />
-        <FirstClass name="Katherine" /> */}
-
-        {choco.map(choco => {
-          return <Cacao chocolat={choco} />
-        })}
+        <Navbar />
+        <Cats />
       </div>  
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import Cats from './components/cats/cats'
+import Navbar from './components/navbar/navbar'
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Cats />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
